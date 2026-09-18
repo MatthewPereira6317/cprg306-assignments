8409ec284c7a7475e7323124a1db13511fd8ecf8
MatthewPereira6317 <Matthew.Pereira@edu.sait.ca>
2026-09-18 00:19:29 -0600

    Update Week 2
    Updated the code for week 2

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,9 @@
+import StudentInfo from "./student-info";
 export default function Page() { 
   return ( 
     <main> 
       <h1>Shopping List</h1> 
+      <StudentInfo name="Matthew Pereira" link="https://github.com/MatthewPereira6317/cprg306-assignments.git" /> 
     </main> 
   ); 
 } 
---
+++ app/week-2/student-info.js
@@ -0,0 +1,8 @@
+export default function StudentInfo({ name, link }) {
+  return (
+    <div>
+      <p>{name}</p>
+      <p>{link}</p>
+    </div>
+  );
+}
